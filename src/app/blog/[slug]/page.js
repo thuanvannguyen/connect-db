62b131cfee907574f5b988b3bbb79f0f551a0a20
thuan.nv <nguyenthuankma@gmail.com>
2024-08-30 11:39:10 +0700

    Update page.js

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -2,11 +2,11 @@ import { getPost } from '@/lib/data';
 import React from 'react'
 
 const Detail = async ({ params }) => {
-  console.log(params);
   const { slug } = params;
+  // console.log(params);
 
   const post = await getPost(slug);
-  console.log(post);
+  // console.log(post);
   return (
     <div>Detail</div>
   )
