a38df27a5e3116f30ea3fa4532259acedf1b2779
thuan.nv <nguyenthuankma@gmail.com>
2024-08-30 15:58:42 +0700

    Update Api router

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,10 +1,22 @@
-import { getPost } from '@/lib/data';
-import React from 'react'
+
+
+// FETCH DATA WITH AN API
+const getData = async (slug) => {
+  const res = await fetch(`http://localhost:3001/api/blog/${slug}`);
+
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  return res.json();
+};
+
 
 const Detail = async ({ params }) => {
   const { slug } = params;
-  const post = await getPost(slug);
+  const post = await getData(slug);
   console.log("Chi tiết", post);
+  // const post = await getPost(slug);
   return (
     <div>Detail</div>
   )
--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,12 +1,24 @@
-import { getPosts } from '@/lib/data'
-import React from 'react'
 
+const getData = async () => {
+  const res = await fetch("http://localhost:3001/api/blog", { next: { revalidate: 3600 } });
+
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  return res.json();
+};
 const Blog = async () => {
-  const posts = await getPosts();
-  console.log("posts", posts);
+  const posts = await getData();
+
+  // const posts = await getPosts();
+  console.log("posts 123", posts);
   return (
     <div>Blog</div>
   )
 }
 
 export default Blog
+
+
+
